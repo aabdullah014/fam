bfc9614bd0346da684a9d0ba46e3cb50befe6f4a
Abdulrahman Abdullah <96795264+aabdullah014@users.noreply.github.com>
2022-02-05 16:40:12 -0700

    Implemented basic CRUD
    Created basic CRUD (Create, Read, Update, Delete) Functionality via RESTful routing

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ mongoose.connect('mongodb://localhost:27017/masjid-finder', {
     useUnifiedTopology: true
 });
 
-const Masjid = require('./models/masjid')
+const methodOverride = require('method-override');
+const Masjid = require('./models/masjid');
 const path = require('path');
 const db = mongoose.connection;
 
@@ -16,21 +17,66 @@ db.once('open', () => {
 
 const app = express();
 
+//set view engine to verify we will use ejs
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+
+//help parse req.body created from post request from new.ejs form
+app.use(express.urlencoded({ extended: true }));
+
+//to incorporate PUT request
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
-app.get('/makeMasjid', async (req, res) => {
-    const m = new masjid({
-        name: 'Hoda',
-        location: 'Gainesville'
-    })
-    await m.save();
-    res.send(m);
+
+//GET route to index of all masajid
+app.get('/masajid', async (req, res) => {
+    const masajid = await Masjid.find({});
+    res.render('masajid/index', { masajid })
 })
 
+//GET route to create a new masjid
+app.get('/masajid/new', (req, res) => {
+    res.render('masajid/new');
+})
+
+//POST route to submit request
+app.post('/masajid', async (req, res) => {
+    const masjid = new Masjid(req.body.masjid);
+    await masjid.save();
+    res.redirect(`/masajid/${masjid._id}`);
+})
+
+//GET route to show page of a masjid
+app.get('/masajid/:id', async (req, res) => {
+    const masjid = await Masjid.findById(req.params.id);
+    res.render('masajid/show', { masjid });
+})
+
+//GET route to edit a masjid
+app.get('/masajid/:id/edit', async (req, res) => {
+    const masjid = await Masjid.findById(req.params.id);
+    res.render('masajid/edit', { masjid });
+})
+
+//PUT request to update masjid
+app.put('/masajid/:id', async (req, res) => {
+    const { id } = req.params;
+    const masjid = await Masjid.findByIdAndUpdate(id, { ...req.body.masjid });
+    res.redirect(`/masajid/${masjid.id}`);
+})
+
+//DELETE request to delete masjid
+app.delete('/masajid/:id', async (req, res) => {
+    const { id } = req.params;
+    await Masjid.findByIdAndDelete(id);
+    res.redirect('/masajid');
+})
+
+
+//verify that server is on and listening
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
